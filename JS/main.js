f5494098dec36761dc53bba0794b70c1e4c6f47d
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-12-08 18:35:30 -0500

    Copied my old menu, made changes

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+ 
+ 
+const ham = document.querySelector('.ham');
+const mIcon = document.querySelector('.mIcon');
+const ul = document.querySelector('ul');
+const nav = document.querySelector('nav');
+const li = document.querySelectorAll('nav li');
+console.log(ham, mIcon, ul, nav, li)
+
+ham.addEventListener('click', animateMenu);
+li.forEach(i => {
+    i.addEventListener('click', toggleMenu);
+})
+
+function toggleMenu() {
+    if(nav.classList.contains('expand') === false) {
+        return;
+    } else {
+        mIcon.classList.toggle('animate');
+        ul.classList.toggle('ulShow');
+        nav.classList.toggle('expand');
+
+    }
+}
+function animateMenu() {
+    console.log('hmm')
+    mIcon.classList.toggle('animate');
+    ul.classList.toggle('ulShow');
+    nav.classList.toggle('expand');
+
+}
