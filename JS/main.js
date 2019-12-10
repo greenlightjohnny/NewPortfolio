a9703d6c52f4aa4eb44b46607357e1dd092abc4c
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-12-10 14:55:37 -0500

    renamed files

--- JS/main.js
+++ JS/main.js
@@ -5,7 +5,7 @@ if ("serviceWorker" in navigator) {
     } else {
       // Register the service worker
       navigator.serviceWorker
-        .register("sw.js", {
+        .register("pwabuilder-sw.js", {
           scope: "./"
         })
         .then(function (reg) {
