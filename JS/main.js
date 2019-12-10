e5ded2460ad9e7d903ca2163b25e396ff608bd5a
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-12-10 16:22:03 -0500

    hmmm

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,8 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js")
-        .then(function (registration) {
-            console.log("Service Worker registered with scope:", 
-                         registration.scope);
-        }).catch(function (err) {
-        console.log("Service worker registration failed:", err);
-    });
-}
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('service worker registered'))
+      .catch(err => console.log('service worker not registered', err));
+  }
 
 const ham = document.querySelector('.ham');
 const mIcon = document.querySelector('.mIcon');
--- sw.js
+++ sw.js
@@ -1,86 +1,37 @@
-const CACHE_NAME = 'vid-voter-v3';
-const URLS_CACHE_ONLY = [
-    "/CSS/style.css",
-    "/JS/main.jus",
-    "/IMG/bquare.jpg",
-    
-    "https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap"
-    
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/JS/main.jss',
+  '/CSS/style.css',
+  '/IMG/bsquare.jpg',
+  'https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap',
 ];
-const URLS_OVER_NETWORK_WITH_CACHE_FALLBACK = [
-    "/CSS/style.css",
-    "/JS/main.jus",
-    "/IMG/bquare.jpg",
-    "/index.html",
-    "https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap"
-];
-self.addEventListener("install", function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(URLS_CACHE_ONLY.concat(URLS_OVER_NETWORK_WITH_CACHE_FALLBACK));
-        }).catch((err) => {
-            console.error(err);
-            return new Promise((_resolve, reject) => {
-                reject('ERROR: ' + err);
-            });
-        })
-    );
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
-self.addEventListener("fetch", function (event) {
-    const requestURL = new URL(event.request.url);
-    if (requestURL.pathname === '/') {
-      event.respondWith(getByNetworkFallingBackByCache("/index.html"));
-    } else if (URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.href) || URLS_OVER_NETWORK_WITH_CACHE_FALLBACK.includes(requestURL.pathname)) {
-        event.respondWith(getByNetworkFallingBackByCache(event.request));
-    } else if (URLS_CACHE_ONLY.includes(requestURL.href) || URLS_CACHE_ONLY.includes(requestURL.pathname)) {
-        event.respondWith(getByCacheOnly(event.request));
-    }
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 });
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.map(function (cacheName) {
-                    if (CACHE_NAME !== cacheName && cacheName.startsWith("vid-voter")) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
-/**
- * 1. We fetch the request over the network
- * 2. If successful we add the new response to the cache
- * 3. If failed we return the result from the cache
- *
- * @param request
- * @param showAlert
- * @returns Promise
- */
-const getByNetworkFallingBackByCache = (request, showAlert = false) => {
-    return caches.open(CACHE_NAME).then((cache) => {
-        return fetch(request).then((networkResponse) => {
-            cache.put(request, networkResponse.clone());
-            return networkResponse;
-        }).catch(() => {
-            if (showAlert) {
-                alert('You are in offline mode. The data may be outdated.')
-            }
-            return caches.match(request);
-        });
-    });
-};
-/**
- * Get from cache
- *
- * @param request
- * @returns Promise
- */
-const getByCacheOnly = (request) => {
-    return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match(request).then((response) => {
-            return response;
-        });
-    });
-};
