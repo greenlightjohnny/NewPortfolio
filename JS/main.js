3bee6d2dfa669d6abfa34c4833bf04b39597af08
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-12-10 15:46:10 -0500

    test

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,13 @@
-// register service worker — BEGINNING
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', {scope: '/'}).then(function(reg) {
-      if(reg.installing) {
-        console.log('Service worker installing');
-      } else if(reg.waiting) {
-        console.log('Service worker installed');
-      } else if(reg.active) {
-        console.log('Service worker active');
-      }
-    }).catch(function(error) {
-      // registration failed
-      console.log('Registration failed with ' + error);
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js")
+        .then(function (registration) {
+            console.log("Service Worker registered with scope:", 
+                         registration.scope);
+        }).catch(function (err) {
+        console.log("Service worker registration failed:", err);
     });
-  }
-  // register service worker — END
+}
+
 const ham = document.querySelector('.ham');
 const mIcon = document.querySelector('.mIcon');
 const ul = document.querySelector('ul');
